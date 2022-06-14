a084526537775f7159d94eed77be755cfac7c9f8
Danai Kafetzaki <danae.kafetzaki@gmail.com>
2022-06-14 09:15:40 +0200

    test-morphotype.R initial commit

--- tests/testhat/test-morphotype.R
+++ tests/testhat/test-morphotype.R
@@ -1,1 +1,9 @@
+test_that("get_input() extracts data matrix from technical drawing image", {
+  expect_equal(get_input(filename='SADR010324.jpg',trim=10,thr=0.9,wd='~/myRpacks/morphotype/inst/extdata'),
+               list("matrix" = image_denoised, "alternative" = NULL))
+})
 
+# test_that("get_input() errors if the file extension is not provided", {
+#   expect_error(get_input(filename='SADR010324',trim=10,thr=0.9,wd='~/myRpacks/morphotype/inst/extdata'),
+#                list("matrix" = image_denoised, "alternative" = NULL))
+# })

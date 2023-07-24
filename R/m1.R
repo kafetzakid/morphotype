8b473b7e099dedfabb59ed758708003bd43f8b2e
Danai Kafetzaki <danae.kafetzaki@gmail.com>
2023-07-24 03:25:22 +0200

    updated m1 and m2 .R

--- R/m1.R
+++ R/m1.R
@@ -1,12 +1,12 @@
 #' Output of get_projection_x
 #'
-#' More about Output of get_projection_x
+#' A profile image data as a vector of values in the x-axis, and its minimum and maximum index value.
 #'
 #' @format A list with 3 items:
 #' \describe{
-#'   \item{xx_range}{description}
-#'   \item{X_min}{description}
-#'   \item{X_max}{description}
+#'   \item{xx_range}{Vector of values in the x-axis}
+#'   \item{X_min}{Vector's minimum index}
+#'   \item{X_max}{Vector's maximum index}
 #' }
 #' @source \url{https://www.dataURL.com}
 "m1"
--- R/m2.R
+++ R/m2.R
@@ -1,12 +1,12 @@
-#' Output of ....
+#' Output of get_projection_y
 #'
-#' More about Output of ...
+#' A profile image data as a vector of values in the y-axis, and its minimum and maximum index value.
 #'
 #' @format A list of 3 items:
 #' \describe{
-#'   \item{Item1}{description}
-#'   \item{Item2}{description}
-#'   \item{Item3}{description}
+#'   \item{xx_range}{Vector of values in the y-axis}
+#'   \item{X_min}{Vector's minimum index}
+#'   \item{X_max}{Vector's maximum index}
 #' }
 #' @source \url{https://www.dataURL.com}
 "m2"
